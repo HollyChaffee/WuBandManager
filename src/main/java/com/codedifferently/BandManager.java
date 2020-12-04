98ee21016d74d5a91315b8a99720a40342954de7
Holly Chaffee <saltdc95@gmail.com>
2020-12-04 15:38:39 -0500

    Completed WuBand Manager

--- src/main/java/com/codedifferently/BandManager.java
+++ src/main/java/com/codedifferently/BandManager.java
@@ -9,55 +9,62 @@ public class BandManager {
     }
 
     public String whileLoop() {
+
         String result = "";
-        // create a `counter`
-        // while `counter` is less than length of array
-            // begin loop
+        int counter = 0;// create a `counter`
+        while (counter < wuMemberArray.length) { // while `counter` is less than length of array
 
-                // use `counter` to identify the `current Person` in the array
-                // get `string Representation` of `currentPerson`
-                // append `stringRepresentation` to `result` variable
+            result += wuMemberArray[counter].toString();
+            counter++;
 
-            // end loop
-        return result;
+        } // use `counter` to identify the `current Person` in the array
+        // get `string Representation` of `currentPerson`
+
+        return result;  // append `stringRepresentation` to `result` variable
+        // end loop
     }
-
 
 
     public String forLoop() {
         String result = "";
-        // identify initial value
-        // identify terminal condition
-        // identify increment
-
-        // use the above clauses to declare for-loop signature
+        for (int i = 0; i < wuMemberArray.length; i++) {
+            result += wuMemberArray[i].toString();
+            // identify initial value
+            // identify terminal condition
+            // identify increment
+            // use the above clauses to declare for-loop signature
             // begin loop
-                // use `counter` to identify the `current Person` in the array
-                // get `string Representation` of `currentPerson`
-                // append `stringRepresentation` to `result` variable
+            // use `counter` to identify the `current Person` in the array
+            // get `string Representation` of `currentPerson`
+            // append `stringRepresentation` to `result` variable
+            // end loop
+        }
+            return result;
+        }
+
+
+
+        public String forEachLoop() {
+            String result = "";
+            for (WuMember wuMember : wuMemberArray) {
+                result += wuMember.toString();
+            }
+            // identify array's type
+            // identify array's variable-name
+
+            // use the above discoveries to declare for-each-loop signature
+            // begin loop
+            // get `string Representation` of `currentPerson`
+            // append `stringRepresentation` to `result` variable
             // end loop
 
-        return result;
+            return result;
+        }
+
+
+        public WuMember[] getwuMemberArray() {
+            return wuMemberArray;
+        }
     }
 
 
-
-    public String forEachLoop() {
-        String result = "";
-        // identify array's type
-        // identify array's variable-name
-
-        // use the above discoveries to declare for-each-loop signature
-            // begin loop
-                // get `string Representation` of `currentPerson`
-                // append `stringRepresentation` to `result` variable
-            // end loop
-
-        return result;
-    }
-
-
-    public WuMember[] getwuMemberArray() {
-        return wuMemberArray;
-    }
-}
